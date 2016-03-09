1ec2112e7c50657db55d2cdb263cf318a3db58b4
waiyi <waiyi@users.noreply.github.com>
2016-03-09 16:00:56 -0700

    Update Violation.py

--- Violation.py
+++ Violation.py
@@ -1,2 +1,4 @@
 # Violation Record
+# This component is used by a police officer to issue a traffic ticket and record the violation. 
+# You may assume that all the information about ticket_type has been loaded in the initial database.
 # Gemma
