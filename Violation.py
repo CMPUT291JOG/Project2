f37b0938550ba881bce2a523d01bc62a5b4cc0b9
waiyi <waiyi@users.noreply.github.com>
2016-03-09 16:04:46 -0700

    Update Violation.py

--- Violation.py
+++ Violation.py
@@ -1,4 +1,15 @@
 # Violation Record
 # This component is used by a police officer to issue a traffic ticket and record the violation. 
 # You may assume that all the information about ticket_type has been loaded in the initial database.
+# assume ticket_type already initialized with:  vtype CHAR(10) and fine number(5,2),
+# Violation as Ticket records should contain:
+#  ticket_no     int,
+#  violator_no   CHAR(15),  
+#  vehicle_id    CHAR(15),
+#  office_no     CHAR(15),
+#  vtype        char(10),
+#  vdate        date,
+#  place        varchar(20),
+#  descriptions varchar(1024),
+
 # Gemma
